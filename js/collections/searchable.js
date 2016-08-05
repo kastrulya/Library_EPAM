1f77f6967d43f7591a9119b38ffbb66427df505b
kastrulya <julia.hrihorieva@gmail.com>
2016-08-05 16:08:40 +0300

    searchable collection

--- js/collections/searchable.js
+++ js/collections/searchable.js
@@ -4,17 +4,29 @@
 
 define([
     'jquery',
-    'undescore'
-], function ($, _) {
-   var Searchable = Backbone.Collection.extend({},{
+    'underscore',
+    'backbone',
+    'models/item'
+], function ($, _, Backbone) {
+    var createSearchQuery = function (searchWord) {
+        var fields = ['author', 'description', 'title'];
+        var query = [];
+        fields.forEach(field => {
+            query.push(field + " like '%" + searchWord + "%'");
+        });
+        query = query.join(' or ');
+        return query;
+    };
+    var Searchable = Backbone.Collection.extend({},{
         search: function(query, options){
             options = options || {};
             var search = $.Deferred();
             var collection = new this([], options);
-            collection.url = _.result(collection, 'url') + 'search';
+            // collection.url = _.result(collection, 'url') + 'where';
+            // collection.url = collection.url;
             var fetch = collection.fetch({
                 data: {
-                    q: query
+                    where: createSearchQuery(query)
                 }
             });
             fetch.done(_.bind(function(){
